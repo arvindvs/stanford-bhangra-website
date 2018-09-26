a64062aee19b30a67276367614073bfbfbb4d0cf
arvindvs <33942304+arvindvs@users.noreply.github.com>
2018-09-26 11:23:00 -0700

    Update Performers section and change YouTube icon URL

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ class App extends Component {
                             <a href="https://www.instagram.com/stanfordbhangra/" target="_blank" className="instagram">Instagram</a>
                         </li>
                         <li className="last">
-                            <a href="https://www.youtube.com/results?search_query=stanford+bhangra" target="_blank" className="youtube">YouTube</a>
+                            <a href="https://www.youtube.com/playlist?list=FLjOa-hKxexlHFUlXKwo5ABg" target="_blank" className="youtube">YouTube</a>
                         </li>
                     </ul>
                 </div>
